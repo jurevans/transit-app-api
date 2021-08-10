1dfa8eb66f771fc0798f12d05d7e1f32b9a2bcf5
Justin Evans <jurevans@gmail.com>
2021-08-10 07:22:12 -0400

    Add ability to update GTFS realtime by specific Route IDs array

--- config/gtfsRealtime.ts
+++ config/gtfsRealtime.ts
@@ -6,31 +6,15 @@ const gtfsRealtime = [
     feedIndex: 1,
     agencyId: 'MTA NYCT',
     feedUrls: [
-      'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs',       // 1,2,3,4,5,6,S
-      'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace',   // A,C,E
-      'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-bdfm',  // B,D,F,M
-      'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-g',     // G
-      'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-jz',    // J,Z
-      'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-nqrw',  // N,Q,R,W
-      'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-l',     // L
-      'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-si',    // SIR
-    ],
-    // Test config, to allow requests for specific routes, as an alternative
-    // to the above. Note: It isn't a requirement that endpoints are grouped
-    // like this, other transit agencies or services may provide only a single
-    // endpoint, so the above config should still be valid!
-    feedUrls2: [
       {
-        routes: ['1', '2', '3', '4', '5', '6', 'GS'],
+        routes: ['1', '2', '3', '4', '5', '6', '7', 'GS'],
         url: 'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs',
       },
       {
-        type: 'route',
         routes: ['A', 'C', 'E'],
         url: 'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace',
       },
       {
-        type: 'route',
         routes: ['B', 'D', 'F', 'M'],
         url: 'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-bdfm',
       },
