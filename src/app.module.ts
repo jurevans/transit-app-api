2d4613674145dd8529bb5e80233cecd3cc94ef44
Justin Evans <jurevans@gmail.com>
2021-08-12 13:18:15 -0400

    Minimal cleanup before PR merge

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,14 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TerminusModule } from '@nestjs/terminus';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection, getConnectionOptions } from 'typeorm';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 import { GtfsModule } from './gtfs/gtfs.module';
 import { GeoModule } from './geo/geo.module';
 import { TransitModule } from './transit/transit.module';
 import { HealthController } from './health/health.controller';
 import { AuthMiddleware } from './middleware/auth.middleware';
 import { AuthModule } from './auth/auth.module';
-
 import ormconfig from '../ormconfig';
 
 TypeOrmModule.forRootAsync({
