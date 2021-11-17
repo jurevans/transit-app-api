9720099c3daf243588caa7aa4e6c5dd193581913
Justin Evans <jurevans@gmail.com>
2021-11-17 05:56:42 -0500

    Implementing unit tests for feed.controller and feed.service

--- src/gtfs/feed/feed.controller.spec.ts
+++ src/gtfs/feed/feed.controller.spec.ts
@@ -1,11 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FeedController } from './feed.controller';
 import { FeedService } from './feed.service';
+import { IStaticFeed } from '../interfaces/feed.interface';
 
 describe('FeedController', () => {
   let controller: FeedController;
 
-  const mockFeedService = {};
+  const mockFeedService = {
+    findOne: jest.fn().mockImplementation((): Promise<IStaticFeed> => {
+      return Promise.resolve(mockFeed);
+    }),
+    findAll: jest.fn().mockImplementation((): Promise<IStaticFeed[]> => {
+      return Promise.resolve([mockFeed]);
+    }),
+  };
+
+  const mockFeed: IStaticFeed = {
+    feedIndex: 1,
+    feedStartDate: '2021-06-27',
+    feedEndDate: '2021-07-24',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -21,5 +35,29 @@ describe('FeedController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return a Feed', async () => {
+    const feedIndex = 1;
+
+    expect(await controller.findOne(feedIndex)).toEqual({
+      feedIndex: 1,
+      feedStartDate: expect.any(String),
+      feedEndDate: expect.any(String),
+    });
+
+    expect(mockFeedService.findOne).toHaveBeenCalledWith({ feedIndex });
+  });
+
+  it('should return all Feeds', async () => {
+    expect(await controller.findAll()).toEqual([
+      {
+        feedIndex: 1,
+        feedStartDate: expect.any(String),
+        feedEndDate: expect.any(String),
+      },
+    ]);
+
+    expect(mockFeedService.findAll).toHaveBeenCalledWith();
   });
 });
--- src/gtfs/feed/feed.service.spec.ts
+++ src/gtfs/feed/feed.service.spec.ts
@@ -2,11 +2,25 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { FeedService } from './feed.service';
 import { FeedInfo } from 'entities/feedInfo.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { IStaticFeed } from '../interfaces/feed.interface';
 
 describe('FeedService', () => {
   let service: FeedService;
 
-  const mockFeedRepository = {};
+  const mockFeedRepository = {
+    findOneOrFail: jest.fn().mockImplementation((): Promise<IStaticFeed> => {
+      return Promise.resolve(mockFeed);
+    }),
+    find: jest.fn().mockImplementation((): Promise<IStaticFeed[]> => {
+      return Promise.resolve([mockFeed]);
+    }),
+  };
+
+  const mockFeed: IStaticFeed = {
+    feedIndex: 1,
+    feedStartDate: '2021-06-27',
+    feedEndDate: '2021-07-24',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -24,5 +38,27 @@ describe('FeedService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return an array of Feeds', async () => {
+    expect(await service.findAll()).toEqual([
+      {
+        feedIndex: 1,
+        feedStartDate: expect.any(String),
+        feedEndDate: expect.any(String),
+      },
+    ]);
+  });
+
+  it('should return a Feed', async () => {
+    const feedIndexProps = {
+      feedIndex: 1,
+    };
+
+    expect(await service.findOne(feedIndexProps)).toEqual({
+      feedIndex: 1,
+      feedStartDate: expect.any(String),
+      feedEndDate: expect.any(String),
+    });
   });
 });
